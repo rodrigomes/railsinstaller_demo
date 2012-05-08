e4a6914c6a61f0732a83642208f0f0549f01270d
Rodrigo da Rocha Gomes <rgomes.rodrigo@gmail.com>
2012-05-08 18:00:53 -0300

    Add mysql2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsinstallerDemo::Application.config.secret_token = 'f7bdd76d0d8ca8b610a16e84e0000f5e052af1ebbf71c086fb1658d7ea07d86a2065ec74922b738a9d1d11d63c16c6cc3741560a262ed3fae32d0fac04add511'
+RailsinstallerDemo::Application.config.secret_token = '4ecddf443e7701d5b6498df1a35f82169047d2f1304af4072aafecbb55e4cde2175d9951babfb8b032bec46f4ede861614d4ca67f8da7480b87451b27087eeeb'
